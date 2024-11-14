3af4c176e95279ee51be32bdc27fc2823106abe2
Muhammet Furkan ALTIN <m.furkangold@gmail.com>
2024-11-14 14:23:44 +0300

    .

---
+++ Csharp/TemelProgramlama/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TemelProgramlama;
+
+public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dizi Boyutunu giriniz: ");
+            int boyut = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0 ;i < length ; i ++)
+            {
+
+            }
+            Console.KeyRead()
+        }
+    }   
