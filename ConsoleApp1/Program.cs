5e8b405392d9eb4fddee8a61585ac9ef5741f6ed
Muhammet Furkan ALTIN <m.furkangold@gmail.com>
2024-11-14 13:00:21 +0300

    deneme

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ProjeAdi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba, Dünya!");
+            Console.ReadKey(); // Programın kapanmasını önler.
+        }
+    }
+}
