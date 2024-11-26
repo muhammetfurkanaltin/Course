29eebbf27dd69c48eba6aab3c81e8dd81b7f508d
Muhammet Furkan ALTIN <m.furkangold@gmail.com>
2024-11-26 20:57:16 +0300

    completed

--- Python/22.SQL VeriTabanı/Okul_Veri_Tabanı.py
+++ Python/22.SQL VeriTabanı/Okul_Veri_Tabanı.py
@@ -2,6 +2,15 @@ import mysql.connector
 from connection import connection
 from datetime import datetime 
 
+ogrenciler=[
+    ("101","Ahmet","Yılmaz",datetime(2005, 5, 17),"E"),
+    ("102","Ali","Can",datetime(2005, 6, 17),"E"),
+    ("103","Canan","Tan",datetime(2005, 7, 7),"K"),
+    ("104","Ayşe","Taner",datetime(2005, 9, 23),"K"),
+    ("105","Bahadır","Toksöz",datetime(2004, 7, 27),"E"),
+    ("106","Ali","Cenk",datetime(2003, 8, 25),"E")
+]
+
 class Student:
     connection = connection
     mycursor = connection.cursor()
@@ -43,17 +52,34 @@ class Student:
 
 #ahmet = Student("131","Ahmet","Yılmaz",datetime(2005, 5, 17),"E")
 #ahmet.saveStudent()
+#Student.saveStudents(ogrenciler)
+    
+    
+    @staticmethod
+    def StudentInfo():
+        sql = "select * from student LIMIT 5"
+        #sql = 'Select StudentNumber,Name,Surname From student '
+        #sql = "select * from student where Gender LIKE '%K%' " 
+        #sql = "select * from student where YEAR(birthdate) = 2003" #MOUTH() DAY()
+        #sql = "select * from student where YEAR(birthdate) = 2005 name = 'Ali' " 
+        #sql = "select * from student where name like '%an%' or surname like '%an%'"
+        #sql = "select COUNT(*) from student where gender = 'E' "
+            #result = Student.mycursor.fetchone()
+        #sql = "select * from student where Gender LIKE '%K%' order by name,surname" 
+
+        Student.mycursor.execute(sql)
+        try:  
+            result = Student.mycursor.fetchall()
+            
+            for product in result :
+               print(f'no: {product[0]} Name: {product[1]} \a Surname: {product[2]} \a Birthdate: {product[3]}')
 
 
-ogrenciler=[
-    ("101","Ahmet","Yılmaz",datetime(2005, 5, 17),"E"),
-    ("102","Ali","Can",datetime(2005, 6, 17),"E"),
-    ("103","Canan","Tan",datetime(2005, 7, 7),"K"),
-    ("104","Ayşe","Taner",datetime(2005, 9, 23),"K"),
-    ("105","Bahadır","Toksöz",datetime(2004, 7, 27),"E"),
-    ("106","Ali","Cenk",datetime(2003, 8, 25),"E")
-]
+        except mysql.connector.Error as err :
+            print("Hata: ", err)
+        finally:
+            Student.connection.close()
 
-Student.saveStudents(ogrenciler)
+Student.StudentInfo()
 
 
